d952f1c573f98021e7b50e23c2f1a535afface56
BrianDingDing <briandwj@icloud.com>
2024-08-02 17:19:17 +0800

    numpy02

--- a_machine_learning_basic/a_numpy/numpy_demo_06.py
+++ a_machine_learning_basic/a_numpy/numpy_demo_06.py
@@ -1,37 +1,30 @@
 """
-    ndarray数组维度操作
-    视图变维, 复制变维: 不会修改原始数据的维度.
-    就地变维: 直接修改原始数据的维度.
+    自定义复合类型
 """
 import numpy as np
+import warnings  # 警告
 
-# 1. 视图变维 (数据共享): reshape() 与 ravel(): 直接拉伸成一维数据.
-ary = np.arange(1, 19)
-bry = ary.reshape(2, 9)
+warnings.filterwarnings('ignore')
 
-bry[0][0] = 99
-print(bry)  # 二维数组
-print(ary)  # 还是一维数组, 原始数据维度不变
+data = [
+    ('zs', [90, 80, 85], 18),
+    ('ls', [92, 81, 83], 19),
+    ('ww', [95, 85, 95], 20)
+]
 
-# 创建一个1,18 shape=(3,6)
-ary2 = np.arange(1, 19).reshape(1, 3, 6)
-print(ary2)
+# 第一种设置dtype类型
+ary = np.array(data, dtype='U2,3int32,int32')
+print(ary)
+print(ary.shape)
 
-# ravel() 不管几维数据, 直接拉伸一维数据
-ary3 = ary2.ravel()
-print(ary3)
+# 求第三列平均值年龄, f=field
+print(ary['f2'].mean())
+# 求每一个人的平均分
+print(ary['f1'].mean(axis=1))
+# 求每科平均分
+print(ary['f1'].mean(axis=0))
 
-# 2. 复制变维 (数据独立): flatten(), 相当于ravel
-print('---' * 30)
-ary4 = np.arange(1, 9).reshape(2, 2, 2)
-bry4 = ary4.flatten()
-
-bry4[0] = 666
-print(bry4)
-print(ary4)
-
-# 3. 就地变维: 直接改变原数组对象的维度, 不返回新数组.
-# 对shape属性进行, 就是就地变维.
-ary5 = np.arange(1, 9)
-ary5.resize(2, 4)
-print(ary5)
+# 第二种设置dtype的方式
+ary2 = np.array(data, dtype={'names': ['name', 'score', 'age'],
+                             'formats': ['U2', '3int32', 'int32']})
+print(ary2['age'])
